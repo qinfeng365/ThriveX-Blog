b434fd7d3b35a42d9423e38d7bc2b391a881c2ae
宇阳 <3311118881@qq.com>
2024-08-27 20:44:33 +0800

    从环境变量获取敏感信息

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        GAODE_KEY_CODE: "455e8ada6799412070c1156c6936b7c4",
+        GAODE_SECURITYJS_CODE: "c8c59309d679d989a8a56461956cdd38",
+    }
+};
 
 export default nextConfig;
