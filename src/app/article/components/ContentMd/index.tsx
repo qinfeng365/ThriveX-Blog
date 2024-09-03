f8d5d1c8668ba354b3f049a6b6bc2e34ea156444
宇阳 <3311118881@qq.com>
2024-09-03 16:16:43 +0800

    图片预览功能

--- src/app/article/components/ContentMd/index.tsx
+++ src/app/article/components/ContentMd/index.tsx
@@ -2,46 +2,66 @@
 
 import React, { useEffect } from "react";
 import ReactMarkdown from "react-markdown";
-import "github-markdown-css"
+import "github-markdown-css";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 import "./index.scss";
 
-export default ({ data }: { data: string }) => {
+interface Props {
+    data: string;
+}
+
+const ContentMdComponent = ({ data }: Props) => {
     useEffect(() => {
-        const images = document.querySelectorAll('img');
+        const images = document.querySelectorAll<HTMLImageElement>('img');
+
         setTimeout(() => {
             images.forEach((img) => {
                 img.style.filter = 'blur(0)';
             });
-        }, 1000)
-    }, [])
+        }, 1000);
+    }, []);
 
     useEffect(() => {
         // 当组件挂载时，改变body背景颜色
         document.body.style.backgroundColor = '#fff';
 
         // 更改波浪颜色
-        const waves = document.querySelectorAll<SVGUseElement>(".waves use")
-        waves[0].style.fill = "rgba(255, 255, 255, 0.7)"
-        waves[1].style.fill = "rgba(255, 255, 255, 0.5)"
-        waves[2].style.fill = "rgba(255, 255, 255, 0.3)"
-        waves[3].style.fill = "rgba(255, 255, 255)"
+        const waves = document.querySelectorAll<SVGUseElement>(".waves use");
+        waves[0].style.fill = "rgba(255, 255, 255, 0.7)";
+        waves[1].style.fill = "rgba(255, 255, 255, 0.5)";
+        waves[2].style.fill = "rgba(255, 255, 255, 0.3)";
+        waves[3].style.fill = "rgba(255, 255, 255)";
 
         // 当组件卸载时，恢复body背景颜色
         return () => {
             document.body.style.backgroundColor = '#f9f9f9';
 
-            waves[0].style.fill = "rgba(249, 249, 249, 0.7)"
-            waves[1].style.fill = "rgba(249, 249, 249, 0.5)"
-            waves[2].style.fill = "rgba(249, 249, 249, 0.3)"
-            waves[3].style.fill = "rgba(249, 249, 249)"
+            waves[0].style.fill = "rgba(249, 249, 249, 0.7)";
+            waves[1].style.fill = "rgba(249, 249, 249, 0.5)";
+            waves[2].style.fill = "rgba(249, 249, 249, 0.3)";
+            waves[3].style.fill = "rgba(249, 249, 249)";
         };
     }, []);
 
+    // 自定义指定标签
+    const renderers = {
+        img: ({ alt, src }: { alt?: string; src?: string }) => (
+            <PhotoView src={src || ''}>
+                <img alt={alt} src={src} />
+            </PhotoView>
+        ),
+    };
+
     return (
         <div className="ContentMdComponent">
-            <div className="content markdown-body">
-                <ReactMarkdown>{data}</ReactMarkdown>
-            </div>
+            <PhotoProvider>
+                <div className="content markdown-body">
+                    <ReactMarkdown components={renderers}>{data}</ReactMarkdown>
+                </div>
+            </PhotoProvider>
         </div>
     );
 };
+
+export default ContentMdComponent;
