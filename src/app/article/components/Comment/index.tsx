97cbd4475e4921b0d7eaa70dfab81f1d4bcc4fd7
宇阳 <3311118881@qq.com>
2024-09-03 15:27:54 +0800

    调整样式

--- src/app/article/components/Comment/index.tsx
+++ src/app/article/components/Comment/index.tsx
@@ -86,23 +86,23 @@ const CommentForm = ({ id }: Props) => {
                         <span className='text-red-400 text-sm pl-3'>{errors.content?.message}</span>
                     </div>
 
-                    <div className='flex flex-col'>
-                        <input type="text" className="ipt w-48 h-9 pl-4" placeholder="你的名称 *" {...register("name", { required: "请输入名称" })} />
+                    <div className='flex flex-col w-[32%]'>
+                        <input type="text" className="ipt w-full h-9 pl-4" placeholder="你的名称" {...register("name", { required: "请输入名称" })} />
                         <span className='text-red-400 text-sm pl-3 mt-1'>{errors.name?.message}</span>
                     </div>
 
-                    <div className='flex flex-col'>
-                        <input type="text" className="ipt w-64 h-9 pl-4" placeholder="你的邮箱（选填）*" {...register("email", { pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, message: "请输入正确的邮箱" } })} />
+                    <div className='flex flex-col w-[32%]'>
+                        <input type="text" className="ipt w-full h-9 pl-4" placeholder="你的邮箱（选填）" {...register("email", { pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, message: "请输入正确的邮箱" } })} />
                         <span className='text-red-400 text-sm pl-3 mt-1'>{errors.email?.message}</span>
                     </div>
 
-                    <div className='flex flex-col'>
-                        <input type="text" className="ipt w-80 h-9 pl-4" placeholder="头像（选填）*" {...register("avatar", { pattern: { value: /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(:\d+)?(\/[^\s]*)?$/, message: "请输入正确的头像链接" } })} />
+                    <div className='flex flex-col w-[32%]'>
+                        <input type="text" className="ipt w-full h-9 pl-4" placeholder="头像（选填）" {...register("avatar", { pattern: { value: /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(:\d+)?(\/[^\s]*)?$/, message: "请输入正确的头像链接" } })} />
                         <span className='text-red-400 text-sm pl-3 mt-1'>{errors.avatar?.message}</span>
                     </div>
 
                     <div className='w-full flex flex-col'>
-                        <input type="text" className="ipt w-full h-9 pl-4" placeholder="你的站点（选填）*"  {...register("url", { pattern: { value: /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(:\d+)?(\/[^\s]*)?$/, message: "请输入正确的网站链接" } })} />
+                        <input type="text" className="ipt w-full h-9 pl-4" placeholder="你的站点（选填）"  {...register("url", { pattern: { value: /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(:\d+)?(\/[^\s]*)?$/, message: "请输入正确的网站链接" } })} />
                         <span className='text-red-400 text-sm pl-3 mt-1'>{errors.url?.message}</span>
                     </div>
 
--- src/app/article/components/UpAndDown/index.tsx
+++ src/app/article/components/UpAndDown/index.tsx
@@ -13,20 +13,20 @@ interface Props {
     next: info,
 };
 
-const btn = "group w-full py-4 mx-3 border hover:border-primary hover:bg-[#f8fbff] transition rounded-md space-y-3"
-const p = "group-hover:text-primary text-xl text-center transition-colors"
+const btnSty = "group w-full py-4 border hover:border-primary hover:bg-[#f8fbff] transition rounded-md"
+const titleSty = "group-hover:text-primary text-xl text-center transition-colors"
 
 const UpAndDown = ({ id, prev, next }: Props) => {
     return (
         <div className='UpAndDownComponent'>
-            <div className="flex justify-between mt-8">
-                <Link href={`/article/${prev ? prev.id : id}`} className={btn}>
-                    <p className={p}>上一篇</p>
+            <div className="flex justify-between mt-8 space-x-3">
+                <Link href={`/article/${prev ? prev.id : id}`} className={btnSty}>
+                    <p className={titleSty}>上一篇</p>
                     <p className='text-center transition-colors'>{prev ? prev.title : '没有上一篇文章了~'}</p>
                 </Link>
 
-                <Link href={`/article/${next ? next.id : id}`} className={btn}>
-                    <p className={p}>下一篇</p>
+                <Link href={`/article/${next ? next.id : id}`} className={btnSty}>
+                    <p className={titleSty}>下一篇</p>
                     <p className='text-center transition-colors'>{next ? next.title : '没有下一篇文章了~'}</p>
                 </Link>
             </div>
