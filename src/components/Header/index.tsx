d91a02ecb46c16e5b0d9a5695b8572ac51aa89d7
宇阳 <3311118881@qq.com>
2024-09-07 15:34:55 +0800

    优化分类样式

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 import Show from '@/components/Show'
@@ -13,9 +14,14 @@ import { getCateListAPI } from '@/api/cate';
 import "./index.scss"
 
 const Header = () => {
+    const patchName = usePathname();
+    // 这些路径段不需要改变导航样式
+    const isPathSty = ['/my'].includes(patchName)
+    // 是否改变导航样式
     const [isScrolled, setIsScrolled] = useState(false);
-    const [cateList, setCateList] = useState<Cate[]>([])
 
+    // 获取分类列表
+    const [cateList, setCateList] = useState<Cate[]>([])
     const getCateList = async () => {
         const { data } = await getCateListAPI()
         setCateList(data)
@@ -43,21 +49,21 @@ const Header = () => {
                         <li className="one_item">
                             <Link href="/" className="one_item_nav">
                                 <img
-                                    src={!isScrolled ? darkLogo.src : lightLogo.src}
+                                    src={isPathSty || isScrolled ? lightLogo.src : darkLogo.src}
                                     alt="Logo"
                                 />
                             </Link>
                         </li>
 
                         <li className="one_item">
-                            <Link href="/" className={`one_item_nav ${isScrolled ? 'text-[#333]' : 'text-white'}`}>
+                            <Link href="/" className={`one_item_nav ${isPathSty || isScrolled ? 'text-[#333]' : 'text-white'}`}>
                                 💎 首页
                             </Link>
                         </li>
 
                         {cateList.map(one => (
                             <li key={one.id} className="one_item">
-                                <Link href={`/cate/${one.id}?name=${one.name}`} className={`one_item_nav ${isScrolled ? 'text-[#333]' : 'text-white'}`}>
+                                <Link href={`/cate/${one.id}?name=${one.name}`} className={`one_item_nav ${isPathSty || isScrolled ? 'text-[#333]' : 'text-white'}`}>
                                     {one.icon} {one.name}
                                     <Show is={!!one.children.length} children={(
                                         <IoIosArrowDown className="ml-2" />
@@ -79,8 +85,14 @@ const Header = () => {
                         ))}
 
                         <li className="one_item">
-                            <Link href="/footprint" className={`one_item_nav ${isScrolled ? 'text-[#333]' : 'text-white'}`}>
+                            <Link href="/footprint" className={`one_item_nav ${isPathSty || isScrolled ? 'text-[#333]' : 'text-white'}`}>
                                 ⛳️ 足迹
+                            </Link>
+                        </li>
+
+                        <li className="one_item">
+                            <Link href="/my" className={`one_item_nav ${isPathSty || isScrolled ? 'text-[#333]' : 'text-white'}`}>
+                                👋 关于我
                             </Link>
                         </li>
                     </ul>
