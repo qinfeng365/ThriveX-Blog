822f5ae9af64e9f13c6d606d7b70eb91eafc8907
宇阳 <3311118881@qq.com>
2024-09-12 20:30:35 +0800

    上下篇样式重构

--- src/app/article/components/UpAndDown/index.tsx
+++ src/app/article/components/UpAndDown/index.tsx
@@ -13,7 +13,7 @@ interface Props {
     next: info,
 };
 
-const btnSty = "group w-full py-4 border hover:border-primary hover:bg-[#f8fbff] transition rounded-md"
+const btnSty = "group w-full py-4 border hover:border-primary hover:bg-[#f8fbff] dark:bg-black-b dark:border-black-b transition rounded-md"
 const titleSty = "group-hover:text-primary text-xl text-center transition-colors"
 
 const UpAndDown = ({ id, prev, next }: Props) => {
@@ -22,12 +22,12 @@ const UpAndDown = ({ id, prev, next }: Props) => {
             <div className="flex justify-between mt-8 space-x-3">
                 <Link href={`/article/${prev ? prev.id : id}`} className={btnSty}>
                     <p className={titleSty}>上一篇</p>
-                    <p className='text-center transition-colors mt-3'>{prev ? prev.title : '没有上一篇文章了~'}</p>
+                    <p className='text-center dark:text-[#8c9ab1] transition-colors mt-3'>{prev ? prev.title : '没有上一篇文章了~'}</p>
                 </Link>
 
                 <Link href={`/article/${next ? next.id : id}`} className={btnSty}>
                     <p className={titleSty}>下一篇</p>
-                    <p className='text-center transition-colors mt-3'>{next ? next.title : '没有下一篇文章了~'}</p>
+                    <p className='text-center dark:text-[#8c9ab1] transition-colors mt-3'>{next ? next.title : '没有下一篇文章了~'}</p>
                 </Link>
             </div>
         </div>
