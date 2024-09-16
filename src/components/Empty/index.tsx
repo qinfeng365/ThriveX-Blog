b1113cd37ec142eb0b8372d4952af02c1d7ad1c4
宇阳 <3311118881@qq.com>
2024-09-16 20:21:14 +0800

    完成进度条loading效果

--- src/components/Empty/index.tsx
+++ src/components/Empty/index.tsx
@@ -1,13 +1,10 @@
 import empty from '@/assets/svg/other/empty.svg'
-import './index.scss';
 
 const Empty = ({ info }: { info: string }) => {
     return (
-        <div className='EmptyComponent py-12'>
-            <div className="w-52 mx-auto mt-5">
-                <img src={empty.src} alt="空状态" />
-                <div className="pt-5 text-center text-gray-700 dark:text-white">{info}</div>
-            </div>
+        <div className="w-52 mx-auto py-12 mt-5">
+            <img src={empty.src} alt="空状态" />
+            <div className="pt-5 text-center text-gray-700 dark:text-white">{info}</div>
         </div>
     );
 };
