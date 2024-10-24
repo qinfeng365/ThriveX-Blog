8dfc329587872bff01b462003ad33466f53e36ca
Liu 宇阳 <liuyuyang1024@yeah.net>
2024-10-24 20:58:59 +0800

    生成文章默认摘要

--- src/components/ArticleLayout/Classics/index.tsx
+++ src/components/ArticleLayout/Classics/index.tsx
@@ -2,7 +2,6 @@ import dayjs from 'dayjs';
 import Link from 'next/link';
 import { randomImage } from '@/utils';
 import { Article } from '@/types/app/article';
-import "./index.scss"
 
 import { RiFireLine } from "react-icons/ri";
 import { IoTimeOutline } from "react-icons/io5";
@@ -10,11 +9,22 @@ import { GoTag } from "react-icons/go";
 import Empty from '@/components/Empty';
 import Show from '@/components/Show';
 
+import "./index.scss"
+
 interface ClassicsProps {
     data: Paginate<Article[]>;
 }
 
 const Classics = ({ data }: ClassicsProps) => {
+    // 生成文章摘要，取前100个字
+    const genArticleInfo = (data: Article) => {
+        if (data.description.trim().length) {
+            return data.description
+        } else {
+            return data.content.slice(0, 100)
+        }
+    }
+
     return (
         <div className='ClassicsComponent'>
             <div className="space-y-4">
@@ -33,7 +43,8 @@ const Classics = ({ data }: ClassicsProps) => {
                         <div className="relative w-full sm:w-[65%] py-5 px-5 sm:px-10 lg:px-5 xl:px-10 z-20">
                             <Link href={`/article/${item.id}`} className='flex flex-col justify-between h-full text-center sm:text-start'>
                                 <h3 className='relative w-full my-2.5 text-white text-lg md:text-xl lg:text-[22px] xl:text-2xl overflow-hidden line-clamp-1'>{item.title}</h3>
-                                <p className='text-[#cecece] text-sm sm:text-[15px] leading-7 sm:indent-8 line-clamp-2 xl:line-clamp-3'>{item.description}</p>
+                                {/* <p className='text-[#cecece] text-sm sm:text-[15px] leading-7 sm:indent-8 line-clamp-2 xl:line-clamp-3'>{item.description}</p> */}
+                                <p className='text-[#cecece] text-sm sm:text-[15px] leading-7 sm:indent-8 line-clamp-2 xl:line-clamp-3'>{genArticleInfo(item)}</p>
 
                                 <div className={`flex ${index % 2 === 0 ? 'sm:justify-start' : 'sm:justify-end'} justify-center pt-5 text-end space-x-4 sm:space-x-8`}>
                                     <div className='flex items-center text-xs text-white'>
