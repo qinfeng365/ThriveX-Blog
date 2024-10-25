f94076490a9dc1b8b7f865cad7db0f42d78088f5
Liu 宇阳 <liuyuyang1024@yeah.net>
2024-10-25 17:07:57 +0800

    完成文章归纳功能

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -1,12 +1,18 @@
 "use client"
 
+import Link from "next/link";
 import { useEffect, useState } from "react"
+
 import { getArticleListAPI } from '@/api/article'
 import { Article } from "@/types/app/article"
+
 import Swiper from "@/components/Swiper";
 import Starry from "@/components/Starry";
 import { Accordion, AccordionItem } from "@nextui-org/react";
+
 import archiving from '@/assets/svg/other/archiving.svg'
+import { AiOutlineEye } from "react-icons/ai";
+
 
 interface MonthData {
     total: number;
@@ -24,7 +30,7 @@ interface YearData {
 const Title = ({ data }: { data: YearData }) => {
     return (
         <div>
-            <div className="text-xl font-sans inline-block textMarkSty">{data.year} 年</div>
+            <div className="text-xl font-sans inline-block textMarkSty">{data.year} 年 {(data.wordCount / 1000) > 50 && '🔥'}</div>
             <div>总共发布了：{data.total} 篇文章</div>
             <div>总字数约：{(data.wordCount / 1000).toFixed(2)}K</div>
         </div>
@@ -38,6 +44,8 @@ export default () => {
         const result = groupByYearAndMonth(data);
         // 从早到晚排序
         result.sort((a, b) => b.year - a.year)
+        console.log(result, 999);
+
         setList(result)
     }
 
@@ -63,6 +71,7 @@ export default () => {
             groupedData[year].month[month].total++;
             groupedData[year].total++;
             groupedData[year].wordCount += wordCount;
+            groupedData[year].month[month].wordCount += wordCount;
         });
 
         return Object.values(groupedData);
@@ -71,10 +80,6 @@ export default () => {
     useEffect(() => {
         getArticleList()
     }, [])
-
-    const defaultContent =
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-
 
     return (
         <>
@@ -91,7 +96,7 @@ export default () => {
             </Swiper>
 
             <div className="w-[1200px] mt-10 mx-auto bg-white p-10 rounded-xl border">
-                <h3 className="flex items-center text-2xl mb-3"><img src={archiving.src} alt="归档" className="w-9 mr-3"/> 文章归纳</h3>
+                <h3 className="flex items-center text-2xl mb-3"><img src={archiving.src} alt="归档" className="w-9 mr-3" /> 文章归纳</h3>
 
                 <Accordion
                     className="[&>hr]:bg-[#eee] !px-0"
@@ -135,7 +140,33 @@ export default () => {
                     {
                         list.map((item, index) => (
                             <AccordionItem key={index} aria-label={item.year + '年'} title={<Title data={item} />}>
-                                {defaultContent}
+                                {
+                                    Object.keys(item.month).map(month => (
+                                        <div className="ml-3">
+                                            <div className="relative border-l border-gray-300">
+                                                <div className="mb-8 ml-4">
+                                                    <div className="absolute w-3 h-3 bg-blue-500 rounded-full -left-1.5 border border-white"></div>
+                                                    <div className="ml-6">
+                                                        <div className="flex items-center space-x-4">
+                                                            <div className="text-2xl text-gray-600">{month}月 {((item.month[month].wordCount / 1000) > 10) && '🔥'}</div>
+                                                            <div>{item.month[month].total}篇文章</div>
+                                                            <div>{(item.month[month].wordCount / 1000).toFixed(2)}K字</div>
+                                                        </div>
+
+                                                        {
+                                                            item.month[month].list.map((article: Article) => (
+                                                                <div className="group flex justify-between py-2">
+                                                                    <Link href={`/article/${article.id}`} target="_blank" className="group-hover:text-primary transition-colors">{article.title}</Link>
+                                                                    <span className="flex items-center min-w-24 text-sm text-white group-hover:text-gray-400 transition-colors"><AiOutlineEye className="mr-1"/>{article.view}</span>
+                                                                </div>
+                                                            ))
+                                                        }
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
                             </AccordionItem>
                         ))
                     }
