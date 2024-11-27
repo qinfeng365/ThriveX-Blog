9bea64f2895be9dcba456b44693deaf5c270bd40
宇阳 <liuyuyang1024@yeah.net>
2024-11-27 19:44:59 +0800

    完成闪念布局

--- src/app/record/components/ImageList/index.tsx
+++ src/app/record/components/ImageList/index.tsx
@@ -1,26 +1,34 @@
 "use client"
 
-import Masonry from "react-masonry-css"
-
-const breakpointColumnsObj = {
-    default: 2,
-    1024: 3,
-    700: 2
-};
+import { PhotoProvider, PhotoView } from "react-photo-view"
+import 'react-photo-view/dist/react-photo-view.css';
 
 export default () => {
+    const list = [
+        "https://bu.dusays.com/2024/10/28/671f7a44631d7.png",
+        "https://bu.dusays.com/2024/09/17/66e97036dddcb.png",
+        "https://bu.dusays.com/2024/09/17/66e97035726ae.png",
+        "https://bu.dusays.com/2024/09/17/66e97031cd456.png"
+    ]
+
     return (
         <>
-            <Masonry
-                breakpointCols={breakpointColumnsObj}
-                className="masonry-grid"
-                columnClassName="masonry-grid_column"
-            >
-                <img src="https://bu.dusays.com/2024/10/28/671f7a44631d7.png" alt="闪念图片" className="mb-3 rounded-lg" />
-                <img src="https://bu.dusays.com/2024/09/17/66e97036dddcb.png" alt="闪念图片" className="mb-3 rounded-lg" />
-                <img src="https://bu.dusays.com/2024/09/17/66e97035726ae.png" alt="闪念图片" className="mb-3 rounded-lg" />
-                <img src="https://bu.dusays.com/2024/09/17/66e97031cd456.png" alt="闪念图片" className="mb-3 rounded-lg" />
-            </Masonry>
+            <div className={`flex justify-center ${list.length && 'mt-4'}`}>
+                <PhotoProvider
+                    speed={() => 800}
+                    easing={(type) => (type === 2 ? 'cubic-bezier(0.36, 0, 0.66, -0.56)' : 'cubic-bezier(0.34, 1.56, 0.64, 1)')}
+                >
+                    <div className={`grid gap-4 ${list.length === 1 ? 'justify-center' : 'grid-cols-2'}`}>
+                        {
+                            list?.map((url, index) => (
+                                <PhotoView key={index} src={url}>
+                                    <img src={url} alt="闪念图片" className="rounded-2xl w-full h-full object-cover cursor-pointer" />
+                                </PhotoView>
+                            ))
+                        }
+                    </div>
+                </PhotoProvider>
+            </div>
         </>
     )
 }
--- src/app/record/page.tsx
+++ src/app/record/page.tsx
@@ -4,32 +4,93 @@ import ImageList from "./components/ImageList"
 export default () => {
   return (
     <>
-      <div className="w-[800px] mx-auto pt-24 pb-32 space-y-2">
-        <div className="flex items-center flex-col p-4 mb-10 border rounded-lg bg-white dark:bg-black-b bg-[url('https://bu.dusays.com/2024/11/27/6746e3ec88c4f.jpg')] bg-no-repeat bg-center bg-cover">
+      <div className="w-full lg:w-[800px] px-6 lg:px-0 mx-auto pt-24 pb-10">
+        <div className="flex items-center flex-col p-4 mb-10 border dark:border-black-b rounded-lg bg-white dark:bg-black-b bg-[url('https://bu.dusays.com/2024/11/27/6746e3ec88c4f.jpg')] bg-no-repeat bg-center bg-cover transition-colors">
           <Image src="https://q1.qlogo.cn/g?b=qq&nk=3311118881&s=640" alt="作者头像" width={80} height={80} className="rounded-full avatar-animation shadow-[5px_11px_30px_20px_rgba(255,255,255,0.3)]" />
           <h2 className="my-2 text-white">👋 Liu 宇阳</h2>
           <h4 className="text-xs text-gray-300">🎯 梦想做一名技术顶尖的架构师，奈何学历太低！</h4>
         </div>
 
-        <div className="flex">
-          <Image src="https://q1.qlogo.cn/g?b=qq&nk=3311118881&s=640" alt="作者头像" width={56} height={56} className="rounded-lg border h-14 mr-2" />
+        <div className="space-y-12">
+          <div className="flex flex-col sm:flex-row">
+            <Image src="https://q1.qlogo.cn/g?b=qq&nk=3311118881&s=640" alt="作者头像" width={56} height={56} className="hidden sm:block rounded-lg border dark:border-black-b h-14 mr-2 transition-colors" />
 
-          <div>
-            <div className="flex items-center my-1.5 ml-4 space-x-4">
-              <h3>宇阳</h3>
-              <span className="text-xs">3天前</span>
+            <div className="flex sm:hidden">
+              <Image src="https://q1.qlogo.cn/g?b=qq&nk=3311118881&s=640" alt="作者头像" width={44} height={44} className="rounded-lg border dark:border-black-b h-11 mr-2 transition-colors" />
+
+              <div className="flex sm:hidden items-center my-1.5 ml-2 space-x-4">
+                <h3>宇阳</h3>
+                <span className="text-xs">3天前</span>
+              </div>
             </div>
 
-            <div className="w-full p-4 border rounded-3xl rounded-tl-none bg-white dark:bg-black-b">
-              <p className=" text-gray-600">
-                今晚在写代码的过程中处理一些用户交互细节的时候有所思：
+            <div className="mt-2 sm:mt-0">
+              <div className="hidden sm:flex items-center my-1.5 ml-4 space-x-4">
+                <h3>宇阳</h3>
+                <span className="text-xs">3天前</span>
+              </div>
 
-                在过去的几个月中我一直在全力开发 Follow 这个项目，出于热爱，出于对产品还有开源的热爱。想方设法的让它变得更好。以至于这样，在过去的几个月中我几乎没有停歇过，即便是休息日。还要去同事负责的模块，去修改那些细节问题，他们不注重的，好像等着我去处理了，我只是个 UI/UX 强迫症，看到不爽的就想改掉而已。但现在转念一想，我只是一个打工人罢了，我也不是 LD，我何必要管这么多，只是拿着一份死工资，以及等待着随时都有可能让你滚蛋的一句话。何必呢不如随他们一样，管好自己的一句三分地就好了。
+              <div className="w-full p-4 border dark:border-black-b rounded-3xl rounded-tl-none bg-white dark:bg-black-b transition-colors">
+                <p className="text-gray-600 dark:text-white transition-colors">
+                  🎉 ThriveX 是一个 Next14 + Spring Boot 的产物，该项目专注于分享技术文章和知识，为技术爱好者和从业者提供一个分享、交流和学习的平台。用户可以在平台上发表自己的技术文章，或浏览其他用户分享的文章，并与他们进行讨论和互动。
+                </p>
 
-                确实也是心累了。
-              </p>
+                <ImageList />
+              </div>
+            </div>
+          </div>
 
-              <div className="flex justify-center mt-4">
+          <div className="flex flex-col sm:flex-row">
+            <Image src="https://q1.qlogo.cn/g?b=qq&nk=3311118881&s=640" alt="作者头像" width={56} height={56} className="hidden sm:block rounded-lg border dark:border-black-b h-14 mr-2 transition-colors" />
+
+            <div className="flex sm:hidden">
+              <Image src="https://q1.qlogo.cn/g?b=qq&nk=3311118881&s=640" alt="作者头像" width={44} height={44} className="rounded-lg border dark:border-black-b h-11 mr-2 transition-colors" />
+
+              <div className="flex sm:hidden items-center my-1.5 ml-2 space-x-4">
+                <h3>宇阳</h3>
+                <span className="text-xs">3天前</span>
+              </div>
+            </div>
+
+            <div className="mt-2 sm:mt-0">
+              <div className="hidden sm:flex items-center my-1.5 ml-4 space-x-4">
+                <h3>宇阳</h3>
+                <span className="text-xs">3天前</span>
+              </div>
+
+              <div className="w-full p-4 border dark:border-black-b rounded-3xl rounded-tl-none bg-white dark:bg-black-b transition-colors">
+                <p className="text-gray-600 dark:text-white transition-colors">
+                  🎉 ThriveX 是一个 Next14 + Spring Boot 的产物，该项目专注于分享技术文章和知识，为技术爱好者和从业者提供一个分享、交流和学习的平台。用户可以在平台上发表自己的技术文章，或浏览其他用户分享的文章，并与他们进行讨论和互动。
+                </p>
+
+                <ImageList />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col sm:flex-row">
+            <Image src="https://q1.qlogo.cn/g?b=qq&nk=3311118881&s=640" alt="作者头像" width={56} height={56} className="hidden sm:block rounded-lg border dark:border-black-b h-14 mr-2 transition-colors" />
+
+            <div className="flex sm:hidden">
+              <Image src="https://q1.qlogo.cn/g?b=qq&nk=3311118881&s=640" alt="作者头像" width={44} height={44} className="rounded-lg border dark:border-black-b h-11 mr-2 transition-colors" />
+
+              <div className="flex sm:hidden items-center my-1.5 ml-2 space-x-4">
+                <h3>宇阳</h3>
+                <span className="text-xs">3天前</span>
+              </div>
+            </div>
+
+            <div className="mt-2 sm:mt-0">
+              <div className="hidden sm:flex items-center my-1.5 ml-4 space-x-4">
+                <h3>宇阳</h3>
+                <span className="text-xs">3天前</span>
+              </div>
+
+              <div className="w-full p-4 border dark:border-black-b rounded-3xl rounded-tl-none bg-white dark:bg-black-b transition-colors">
+                <p className="text-gray-600 dark:text-white transition-colors">
+                  🎉 ThriveX 是一个 Next14 + Spring Boot 的产物，该项目专注于分享技术文章和知识，为技术爱好者和从业者提供一个分享、交流和学习的平台。用户可以在平台上发表自己的技术文章，或浏览其他用户分享的文章，并与他们进行讨论和互动。
+                </p>
+
                 <ImageList />
               </div>
             </div>
