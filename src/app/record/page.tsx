af5f2dbe600c3815c37874fcf64eb1a77afa55ed
宇阳 <liuyuyang1024@yeah.net>
2024-12-03 14:46:19 +0800

    调整：已知问题

--- src/app/record/page.tsx
+++ src/app/record/page.tsx
@@ -24,20 +24,19 @@ export default async ({ searchParams }: Props) => {
       <div className="bg-[linear-gradient(to_right,#fff1eb_0%,#d0edfb_100%)] dark:bg-[linear-gradient(to_right,#232931_0%,#232931_100%)]">
         <div className="w-full lg:w-[800px] px-6 lg:px-0 mx-auto pt-24 pb-10">
           <div className="flex items-center flex-col p-4 mb-10 border dark:border-black-b rounded-lg bg-white dark:bg-black-b bg-[url('https://bu.dusays.com/2024/11/27/6746e3ec88c4f.jpg')] bg-no-repeat bg-center bg-cover transition-colors">
-            <Image src={user.avatar} alt="作者头像" width={80} height={80} className="rounded-full avatar-animation shadow-[5px_11px_30px_20px_rgba(255,255,255,0.3)]" />
+            <img src={user.avatar} alt="作者头像" width={80} height={80} className="rounded-full avatar-animation shadow-[5px_11px_30px_20px_rgba(255,255,255,0.3)]" />
             <h2 className="my-2 text-white">👋 Liu 宇阳</h2>
             <h4 className="text-xs text-gray-300">🎯 梦想做一名技术顶尖的架构师，奈何学历太低！</h4>
           </div>
-
 
           <div className="space-y-12">
             {
               !!record?.result?.length && record?.result.map(item => (
                 <div key={item.id} className="flex flex-col sm:flex-row">
-                  <Image src={user.avatar} alt="作者头像" width={56} height={56} className="hidden sm:block rounded-lg border dark:border-black-b h-14 mr-2 transition-colors" />
+                  <img src={user.avatar} alt="作者头像" width={56} height={56} className="hidden sm:block rounded-lg border dark:border-black-b h-14 mr-2 transition-colors" />
 
                   <div className="flex sm:hidden">
-                    <Image src={user.avatar} alt="作者头像" width={44} height={44} className="rounded-lg border dark:border-black-b h-11 mr-2 transition-colors" />
+                    <img src={user.avatar} alt="作者头像" width={44} height={44} className="rounded-lg border dark:border-black-b h-11 mr-2 transition-colors" />
 
                     <div className="flex sm:hidden items-center my-1.5 ml-2 space-x-4">
                       <h3>{user.name}</h3>
