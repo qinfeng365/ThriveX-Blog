b2267dd344877fa7772eb9785d75c5444e7af849
宇阳 <liuyuyang1024@yeah.net>
2024-12-10 15:43:45 +0800

    BUG：修复移动端左侧菜单显示不全问题

--- src/components/Header/component/SidebarNav/index.tsx
+++ src/components/Header/component/SidebarNav/index.tsx
@@ -7,13 +7,13 @@ interface Props {
   list: Cate[]
   open: boolean
   onClose: () => void
-}
+} 
 
 export default ({ list, open, onClose }: Props) => {
   return (
     <>
       <div className={`flex fixed top-0 left-0 ${open ? 'w-full' : 'w-0'} h-full z-[60] transition-width`}>
-        <div className={`overflow-hidden ${open ? 'w-8/12 p-5' : 'w-0'} border-r dark:border-[#2b333e] bg-[rgba(255,255,255,0.9)] dark:bg-[rgba(44,51,62,0.9)] backdrop-blur-[5px] transition-width`}>
+        <div className={`overflow-auto ${open ? 'w-8/12 p-5' : 'w-0'} border-r dark:border-[#2b333e] bg-[rgba(255,255,255,0.9)] dark:bg-[rgba(44,51,62,0.9)] backdrop-blur-[5px] transition-width hide_sliding`}>
           <ul className="flex flex-col space-y-2">
             {list?.map(one => (
               <li key={one.id} className="group/one relative hover:bg-[#e0e6ec] dark:hover:bg-[#495362] rounded-md transition-colors">
