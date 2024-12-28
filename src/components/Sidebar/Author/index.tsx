c1652d710db1d408615175526d33c61f68db4fbc
宇阳 <liuyuyang1024@yeah.net>
2024-12-29 00:43:24 +0800

    小调整

--- src/components/Sidebar/Author/index.tsx
+++ src/components/Sidebar/Author/index.tsx
@@ -16,7 +16,6 @@ import { Social, Theme } from '@/types/app/project';
 
 const Author = async () => {
     const { data: user } = await getUserDataAPI() || { data: {} as User }
-    
     const { data: { social } } = await getConfigDataAPI<Theme>("layout") || { data: {} as Theme }
 
     const socialList = JSON.parse(social)?.map((item: string) => JSON.parse(item))
