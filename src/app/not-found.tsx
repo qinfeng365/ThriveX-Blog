2f3adec4fbcc9113103002dc9aa7ccdb86a623ac
宇阳 <liuyuyang1024@yeah.net>
2024-12-31 17:59:34 +0800

    调整404页面

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,24 @@
 'use client'
 
 import Image from 'next/image'
-import NotFound from '@/assets/image/404.png'
+import NotFoundSvg from '@/assets/svg/other/404.svg'
 
-function NotFoundPage() {
-    return (
-        <html>
-            <body className='bg-white'>
-                <div className='mt-24 mx-auto flex flex-col items-center'>
-                    <h1 className='text-[3vw] font-medium mb-8'>Page not found!</h1>
-                    <Image src={NotFound} alt="" className='w-[35vw]' />
-                </div>
-            </body>
-        </html>
-    )
+export default function NotFound() {
+  return (
+    <>
+      <div className='absolute w-screen h-screen bg-white z-[999]'>
+        <div className="w-full h-[73vh] mt-20">
+          <div className="w-full h-full flex justify-center items-center flex-wrap">
+            <Image src={NotFoundSvg} alt="404" className='w-[40rem] min-w-[20rem]' />
+
+            <div>
+              <h1 className='text-8xl font-bold'>404</h1>
+              <h2 className='text-3xl font-bold my-4'>Page not found</h2>
+              <p>The page you are looking for does not exist or has been removed.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-
-export default NotFoundPage
